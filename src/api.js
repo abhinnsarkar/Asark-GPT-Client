9c86780c3fb435b98098c58d83f20d35605d9182
Abhinn Sarkar <abhinnsarkar@Abhinns-MacBook-Pro.local>
2023-08-08 13:19:22 +0530

    undid server call change

--- src/api.js
+++ src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-    baseURL: process.env.SERVER,
+    // baseURL: process.env.SERVER,
+    baseURL: "https://asark-gpt-backend.onrender.com",
     timeout: 5000,
     headers: {
         "Content-Type": "application/json",
